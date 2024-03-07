95ec9bc2fb5320c1af98abb753d7b238450038e8
alexvieru1 <vieru.alexandruu@gmail.com>
2024-03-07 16:44:37 +0200

    layout 3 almost done

--- app/themes/theme3/page.tsx
+++ app/themes/theme3/page.tsx
@@ -1,11 +1,20 @@
+import CoreValues from "./components/CoreValues";
+import Despre from "./components/Despre";
+import Hero from "./components/Hero";
+import Navbar from "./components/NavBar";
+import ServiciiOferite from "./components/ServiciiOferite";
 
-
-const Theme2 = () => {
+const Theme3 = () => {
   return (
-    <main className=''>
-      NEW
+    <main className="">
+      <Navbar />
+      <Hero />
+      <Despre/>
+      <CoreValues/>
+      <ServiciiOferite/>
+      <div className="h-[3000px]">NEW</div>
     </main>
-  )
-}
+  );
+};
 
-export default Theme2
+export default Theme3;
