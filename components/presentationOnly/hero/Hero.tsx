51132c39d1e4f798f33b2ae05b5b112f2d4cb48f
Rzvone <bobonea.razvan.ctin@gmail.com>
2024-03-21 12:44:12 +0200

    added prisma generate to build

--- components/presentationOnly/hero/Hero.tsx
+++ components/presentationOnly/hero/Hero.tsx
@@ -13,20 +13,19 @@ const Hero = () => {
       />
       <div className="flex flex-col items-center">
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          <h1 className="text-4xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             Clean Code <br />
+            <span className="text-md">Presents Servicii Funerare</span>
           </h1>
         </div>
         <div className="mt-10 flex flex-row gap-10">
-            <Link href="/themes">
-              <Button>
-                Themes
-              </Button>
-            </Link>
-            <Button>
-              <Link href="/shop">Shop</Link>
-            </Button>
-          </div>
+          <Link href="/themes">
+            <Button>Themes</Button>
+          </Link>
+          <Button>
+            <Link href="/shop">Shop</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
